59a39dd9d88ec27eb7d6abc93369db8ad3dd1ca6
rodion <rodion@debian.deb.org>
2018-10-03 23:05:09 +0300

    new projects structure

---
+++ projects/socialdevelop/client/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    devServer: {
+        disableHostCheck: true   // удаление ошибки Invalid Host header при использовани обраного прокси
+    }    
+}
--- up.sh
+++ up.sh
@@ -4,16 +4,18 @@ docker network create proxy_network
 docker network create mysql_network
 docker network create mongo_network
 
-docker network create socialdevelop-client-network
-docker network create socialdevelop-server-network
-docker network create secondtest-client-network
-
 cd dbs/mysql && docker-compose up -d
 cd ../mongo && docker-compose up -d
 cd ../adminer && docker-compose up -d
 
-cd ../../socialdevelop/client && docker-compose up -d
-cd ../../socialdevelop/server && docker-compose up -d
-cd ../../secondtest/client && docker-compose up -d
+cd ../../projects/
 
-cd ../../reverse_proxy && docker-compose up -d
+cd oreshek/client && docker-compose -p oreshek1 up -d
+cd ../server && docker-compose -p oreshek2 up -d
+
+cd ../../socialdevelop/client && docker-compose -p socialdevelop1 up -d
+cd ../server && docker-compose -p socialdevelop2 up -d
+
+cd ../../simple/client && docker-compose -p simple1 up -d
+
+cd ../../../reverse_proxy && docker-compose up -d
